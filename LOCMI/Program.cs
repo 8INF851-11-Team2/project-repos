de439447b6c996fa93891e9b94e01c4ea507ec40
EtienneLF <51823054+EtienneLF@users.noreply.github.com>
2022-10-31 17:06:04 -0400

    Minor change Program.cs

--- LOCMI/Program.cs
+++ LOCMI/Program.cs
@@ -4,28 +4,33 @@ using LOCMI.Core;
 using LOCMI.Microcontrollers;
 using LOCMI.Test;
 
-Console.WriteLine("Hello, World!");
-
+//Build Microcontroller
 var builder = new MicrocontrollerABuilder();
 Microcontroller mc = builder.GetResult();
 
+//Init TestSuite
 TestSuite suite = new TestSuite();
 ITest testA = new TestCaseA("TestCaseA");
 ITest testB = new TestCaseB("TestCaseB");
 suite.AddTest(testA);
 suite.AddTest(testB);
 
+//Certificate generation
 Certificate c = new Certificate(suite, mc, "CertificatA");
 c.Certifier();
 ITestResult impl = c.TestResult;
 
-Console.WriteLine("Failure compt : " + impl.GetFailureCount());
-Console.WriteLine("Run compt : " + impl.GetRunCount());
+//Print
+Console.WriteLine("Failure count : " + impl.GetFailureCount());
+Console.WriteLine("Run count : " + impl.GetRunCount());
 Console.WriteLine("Result certifier : " + c.IsSucces);
 
+//Print Failure 
 ITestResult testResult = c.TestResult;
 List<TestFailure> list = testResult.TestFailures;
 foreach(TestFailure test in list)
 {
     Console.WriteLine("Failure on test : " + test.TestCase.Name + ", reason : " + test.Cause);
 }
+
+Thread.Sleep(5000);
