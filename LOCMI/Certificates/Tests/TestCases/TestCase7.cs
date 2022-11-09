33a88ece2b9b43e3686c5842bd0edbbeac6894f5
syukoGit <syukodev@outlook.com>
2022-11-09 17:38:26 -0500

    Add Test 7

---
+++ LOCMI/Certificates/Tests/TestCases/TestCase7.cs
@@ -0,0 +1,24 @@
+﻿namespace LOCMI.Certificates.Tests.TestCases;
+
+using LOCMI.Core;
+
+public class TestCase7 : TestCase
+{
+    public TestCase7()
+        : base("Has hard disk")
+    {
+    }
+
+    /// <inheritdoc />
+    public override void Run(ITestResult testResult, Microcontroller mc)
+    {
+        if (mc.Disk != null)
+        {
+            testResult.IncrementRunCounter();
+        }
+        else
+        {
+            testResult.AddFailure(this, new[] { "The microcontroller hasn't hard disk" });
+        }
+    }
+}
