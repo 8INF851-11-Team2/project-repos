87aa6ed355304a5a141483cfdb95d683efd6a7dc
syukoGit <syukodev@outlook.com>
2022-11-10 22:46:37 -0500

    Add Test 5

--- LOCMI/Certificates/Tests/TestCases/TestCase4.cs
+++ LOCMI/Certificates/Tests/TestCases/TestCase4.cs
@@ -20,14 +20,14 @@ public class TestCase4 : TestCase
             if (MandatoryConnectors.Any())
             {
                 string mandatoryConnectors = string.Join(" / ", MandatoryConnectors.Select(static c => c.Name));
-                yield return $"The microcontroller hasn't connectors but it must have these connectors: {mandatoryConnectors}";
+                yield return $"The microcontroller hasn't connector but it must have these connectors: {mandatoryConnectors}";
             }
         }
         else
         {
-            IEnumerable<Connector> missingConnector = MandatoryConnectors.Where(connector => !microcontroller.Connectors.Contains(connector));
+            IEnumerable<Connector> missingConnectors = MandatoryConnectors.Where(connector => !microcontroller.Connectors.Contains(connector));
 
-            foreach (Connector connector in missingConnector)
+            foreach (Connector connector in missingConnectors)
             {
                 yield return $"The microcontroller must have the {connector.Name} connector";
             }
---
+++ LOCMI/Certificates/Tests/TestCases/TestCase5.cs
@@ -0,0 +1,37 @@
+﻿namespace LOCMI.Certificates.Tests.TestCases;
+
+using LOCMI.Core;
+using LOCMI.Core.Utils;
+
+public class TestCase5 : TestCase
+{
+    /// <inheritdoc />
+    public TestCase5()
+        : base("Languages")
+    {
+    }
+
+    public IEnumerable<Language> MandatoryLanguages { get; set; } = new List<Language>();
+
+    /// <inheritdoc />
+    protected override IEnumerable<string> Test(Microcontroller microcontroller)
+    {
+        if (microcontroller.Languages == null)
+        {
+            if (MandatoryLanguages.Any())
+            {
+                string mandatoryLanguages = string.Join(" / ", MandatoryLanguages.Select(static c => c.Name));
+                yield return $"The microcontroller hasn't language but it must have these languages: {mandatoryLanguages}";
+            }
+        }
+        else
+        {
+            IEnumerable<Language> missingLanguages = MandatoryLanguages.Where(c => !microcontroller.Languages.Contains(c));
+
+            foreach (Language language in missingLanguages)
+            {
+                yield return $"The microcontroller must have the {language.Name} language with the {language.Version}";
+            }
+        }
+    }
+}
