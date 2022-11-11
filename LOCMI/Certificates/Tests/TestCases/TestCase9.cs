5b3d38a436cc500e1990479f8318ba40345f1b2a
syukoGit <syukodev@outlook.com>
2022-11-11 00:15:41 -0500

    Add Test 9

---
+++ LOCMI/Certificates/Tests/TestCases/TestCase9.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="LOCMI" file="TestCase9.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace LOCMI.Certificates.Tests.TestCases;
+
+using LOCMI.Core;
+
+public class TestCase9 : TestCase
+{
+    public TestCase9()
+        : base("Maintenance testing")
+    {
+    }
+
+    /// <inheritdoc />
+    protected override IEnumerable<string> Test(Microcontroller microcontroller)
+    {
+    }
+}
