9a25b5e0da38c0c487f2e5b205e88ca686f8ebd8
Noémie Lempereur <60352718+Noemie-Lempereur@users.noreply.github.com>
2022-11-13 09:19:07 -0500

    Update View.cs
    Modification du titre au lancement

--- LOCMI/Views/View.cs
+++ LOCMI/Views/View.cs
@@ -4,11 +4,19 @@ public sealed class View
 {
     public View()
     {
+        /*
         Console.WriteLine(@"        _                                _             _ _               __        __ _                          ");
         Console.WriteLine(@"  /\/\ (_) ___ _ __ ___   ___ ___  _ __ | |_ _ __ ___ | | | ___ _ __    / _\ ___  / _| |___      ____ _ _ __ ___ ");
         Console.WriteLine(@" /    \| |/ __| '__/ _ \ / __/ _ \| '_ \| __| '__/ _ \| | |/ _ | '__|   \ \ / _ \| |_| __\ \ /\ / / _` | '__/ _ \");
         Console.WriteLine(@"/ /\/\ | | (__| | | (_) | (_| (_) | | | | |_| | | (_) | | |  __| |      _\ | (_) |  _| |_ \ V  V | (_| | | |  __/");
         Console.WriteLine(@"\/    \|_|\___|_|  \___/ \___\___/|_| |_|\__|_|  \___/|_|_|\___|_|      \__/\___/|_|  \__| \_/\_/ \__,_|_|  \___|");
+        Console.WriteLine(@"                                                                                                                 ");
+        */
+        Console.WriteLine(@" _                          _  ");
+        Console.WriteLine(@"| |    ___   ___    _  _   (_) ");
+        Console.WriteLine(@"| |   / _ \ / __|  / \/ \  | | ");
+        Console.WriteLine(@"| |__| (_) | (__  / Ʌ  Ʌ \ | | ");
+        Console.WriteLine(@"|____|\___/ \___|/_/ \/ \_\|_| ");
         Console.WriteLine(@"                                                                                                                 ");
     }
 
