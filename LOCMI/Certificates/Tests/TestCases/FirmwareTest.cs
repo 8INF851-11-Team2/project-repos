ac9582af36b446b1d10bc9ce14b53aebb8d5464d
syukoGit <syukodev@outlook.com>
2022-11-14 23:57:09 -0500

    Add FirmwareTest

---
+++ LOCMI/Certificates/Tests/TestCases/FirmwareTest.cs
@@ -0,0 +1,17 @@
+﻿namespace LOCMI.Certificates.Tests.TestCases;
+
+using LOCMI.Core;
+
+public class FirmwareTest : TestCase
+{
+    public FirmwareTest()
+        : base("Firmware test")
+    {
+    }
+
+    /// <inheritdoc />
+    protected override IEnumerable<string> Test(Microcontroller microcontroller)
+    {
+        throw new NotImplementedException();
+    }
+}
