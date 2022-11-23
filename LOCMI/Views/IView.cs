6286c50750894adeb98c230a353bd53319a31693
syukoGit <syukodev@outlook.com>
2022-11-23 16:36:31 -0500

    IView can now to display text with color

--- LOCMI/Views/IView.cs
+++ LOCMI/Views/IView.cs
@@ -1,10 +1,16 @@
 namespace LOCMI.Views;
 
+using System.Drawing;
+
 public interface IView
 {
     public void Clear();
 
     public void Display(string message);
+
+    public void Display(string message, Color textColor);
+
+    public void Display(string message, Color textColor, Color backColor);
 
     public string? GetUserEntry();
 }
