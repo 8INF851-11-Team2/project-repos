cf122f800b61795b3d3902312b1cd7ff7ce3e75d
syukoGit <syukodev@outlook.com>
2022-11-23 18:48:53 -0500

    Add ExternalClassLoader

---
+++ LOCMI/Core/Loaders/ExternalClassLoader.cs
@@ -0,0 +1,33 @@
+﻿namespace LOCMI.Core.Loaders;
+
+using System.Reflection;
+
+public sealed class ExternalClassLoader<T> : ILoader<T> where T : class
+{
+    /// <inheritdoc />
+    public T? Load(string path)
+    {
+        Assembly assembly = Assembly.LoadFile(path);
+
+        Type type = typeof(T);
+
+        Type? externType = assembly.GetExportedTypes().FirstOrDefault(c => c.BaseType == type);
+
+        return externType != null
+                   ? Activator.CreateInstance(externType) as T
+                   : null;
+    }
+
+    private T? LoadDll(string path)
+    {
+        Assembly assembly = Assembly.LoadFile(path);
+
+        Type type = typeof(T);
+
+        Type? externType = assembly.GetExportedTypes().FirstOrDefault(c => c.BaseType == type);
+
+        return externType != null
+                   ? Activator.CreateInstance(externType) as T
+                   : null;
+    }
+}
