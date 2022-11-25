bac20bdeaef09f212354735bced323d3987f4692
syukoGit <syukodev@outlook.com>
2022-11-24 21:12:48 -0500

    Set specifications for MicrocontrollerC

--- LOCMI/Microcontrollers/MicrocontrollerC.cs
+++ LOCMI/Microcontrollers/MicrocontrollerC.cs
@@ -1,32 +1,46 @@
 ﻿namespace LOCMI.Microcontrollers;
 
 using LOCMI.Core.Microcontrollers.Utils;
-using LOCMI.Core.Microcontrollers.Utils.PortTypes;
 
 public static class MicrocontrollerC
 {
-    public static IEnumerable<Connector> Connectors => new List<Connector>
+    public const string Connector = "MicroUSB";
+
+    public const double Height = 0.68;
+
+    public const double Length = 1.72;
+
+    public const string Name = "MicrocontrollerC";
+
+    public const int Weight = 82;
+
+    public const double Width = 3.8;
+
+    public static readonly Disk Disk = new ();
+
+    public static readonly Dictionary<int, string> GPIO = new ()
     {
-        new ("USB-C"),
-        new ("Ethernet"),
+        { 1, "VIN" },
+        { 2, "GRN" },
+        { 3, "DATA" },
+        { 4, "DATA" },
+        { 5, "DATA" },
+        { 6, "DATA" },
+        { 7, "OTHER" },
+        { 8, "GRN" },
+        { 9, "DATA" },
+        { 10, "DATA" },
+        { 11, "DATA" },
+        { 12, "OTHER" },
     };
 
-    public static Dimension Dimension => new (10, 20, 30, 40);
+    public static readonly (string Brand, string Model) Identification = ("Arduino", "AR1240");
 
-    public static Identification Identification => new ("NXP", "LPC5500 Series");
+    public static readonly (string Language, string Version) Language = ("C++", string.Empty);
 
-    public static IEnumerable<Language> Languages => new List<Language>
+    public static readonly Dictionary<int, double> Powers = new ()
     {
-        new ("java", "17.0"),
-    };
-
-    public static string Name => "MicrocontrollerC";
-
-    public static Ports Ports => new ()
-    {
-        { 0, new GroundPort() },
-        { 1, new PowerPort(3.3) },
-        { 2, new DataPort() },
-        { 3, new GroundPort() },
+        { 1, 3.3 },
+        { 7, 5 },
     };
 }
