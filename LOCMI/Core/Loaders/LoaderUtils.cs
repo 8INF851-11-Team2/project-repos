5c1f084710b1cf6c73df9a5edf17ebfbb3d5e4c9
EtienneLF <51823054+EtienneLF@users.noreply.github.com>
2022-11-27 00:48:28 -0500

    Modif LoaderUtils

---
+++ LOCMI/Core/Loaders/LoaderUtils.cs
@@ -0,0 +1,23 @@
+﻿namespace LOCMI.Core.Loaders;
+
+using LOCMI.Core.Certificates;
+using LOCMI.Core.Certificates.Tests;
+using LOCMI.Views;
+
+public static class LoaderUtils
+{
+    public static ILoader<T1> GetSameLoader<T1, T2>(ILoader<T2> loader) 
+        where T1 : class 
+        where T2 : class
+    {
+        ILoader<T1> newLoader = loader switch
+        {
+            JsonLoader<T2> json => new JsonLoader<T1>(),
+            ExternalClassLoader<T2> external => new ExternalClassLoader<T1>(),
+            _ => new JsonLoader<T1>(),
+        };
+
+        return newLoader;
+    }
+
+}
