e360823f2a549290ac7a121df4b63ed378901458
syukoGit <syukodev@outlook.com>
2022-11-30 12:56:25 -0500

    Remove IEnumerable to ElectronicVersatilityTest

--- LOCMI/Core/Certificates/Tests/TestCases/ElectronicVersatilityTest.cs
+++ LOCMI/Core/Certificates/Tests/TestCases/ElectronicVersatilityTest.cs
@@ -1,6 +1,6 @@
 ﻿namespace LOCMI.Core.Certificates.Tests.TestCases;
 
-using System.Collections;
+using System.Text.Json.Serialization;
 using LOCMI.Core.Microcontrollers;
 using LOCMI.Core.Microcontrollers.Utils.PortTypes;
 
@@ -8,25 +8,21 @@ using LOCMI.Core.Microcontrollers.Utils.PortTypes;
 ///     Test if the microcontroller supports different voltages.
 /// </summary>
 /// <remarks>Test 1</remarks>
-public sealed class ElectronicVersatilityTest : TestCase, IEnumerable<double>
+public sealed class ElectronicVersatilityTest : TestCase
 {
-    private readonly List<double> _testedVoltage = new ();
-
+    [JsonConstructor]
     public ElectronicVersatilityTest()
         : base("Electronic versatility")
     {
     }
 
-    public void Add(double voltage)
+    public ElectronicVersatilityTest(params double[] voltages)
+        : this()
     {
-        _testedVoltage.Add(voltage);
+        TestedVoltage = voltages.ToList();
     }
 
-    /// <inheritdoc />
-    public IEnumerator<double> GetEnumerator()
-    {
-        return _testedVoltage.GetEnumerator();
-    }
+    public List<double> TestedVoltage { get; set; } = new ();
 
     /// <inheritdoc />
     protected override IEnumerable<string> Test(Microcontroller microcontroller)
@@ -39,14 +35,8 @@ public sealed class ElectronicVersatilityTest : TestCase, IEnumerable<double>
         IEnumerable<PowerPort> powerPorts = microcontroller.Ports.Where(static c => c is PowerPort).Cast<PowerPort>();
 
         // ReSharper disable once CompareOfFloatsByEqualityOperator
-        return _testedVoltage.Where(v => powerPorts.All(p => p.Voltage != v))
-                             .Select(static v => $"The microcontroller does not support an electrical voltage of {v}V")
-                             .ToList();
-    }
-
-    /// <inheritdoc />
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
+        return TestedVoltage.Where(v => powerPorts.All(p => p.Voltage != v))
+                            .Select(static v => $"The microcontroller does not support an electrical voltage of {v}V")
+                            .ToList();
     }
 }
